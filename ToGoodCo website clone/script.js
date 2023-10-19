d4b1f238cc4ca6ff3afc14ec1c6ff57c3fc32e3d
Aniket <aniketraj28042003@gmail.com>
2023-10-19 17:14:35 +0530

    added codes

---
+++ ToGoodCo website clone/script.js
@@ -0,0 +1,59 @@
+const scroll = new LocomotiveScroll({
+  el: document.querySelector(".main"),
+  smooth: true,
+});
+
+function videoAnimation() {
+  let playCursor = document.querySelector("#play");
+  let video = document.querySelector(".videoContainer");
+
+  video.addEventListener("mouseenter", () => {
+    gsap.to(playCursor, {
+      scale: 1,
+      display: "block",
+    });
+  });
+
+  video.addEventListener("mouseleave", () => {
+    gsap.to(playCursor, {
+      scale: 0,
+      display: "none",
+    });
+  });
+
+  video.addEventListener("mousemove", (dets) => {
+    gsap.to(playCursor, {
+      left: dets.x - 40,
+      top: dets.y - 40,
+    });
+  });
+}
+videoAnimation();
+
+function textAnimation() {
+  let tl = gsap.timeline();
+
+  tl.from("h1", {
+    y: 500,
+    duration: 0.6,
+    stagger: 0.3,
+  });
+  tl.from(".videoContainer", {
+    y: 500,
+    scale: 0,
+    duration: 0.5,
+  });
+}
+
+textAnimation();
+
+let shoping = document.querySelector("#page3");
+
+shoping.addEventListener("mousemove", (d) => {
+  gsap.to(".cursor", {
+    left: d.x,
+    top: d.y,
+  });
+});
+
+// document.querySelector()
